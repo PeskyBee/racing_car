570b8d56210d8dabf530fe285cd606a956f3bf9d
Anton Kozlov <tonickkozlov@me.com>
2017-04-08 15:35:56 +0300

    [fix] Fixed delay between commands

--- arduino/serial_protocol/serial_protocol.ino
+++ arduino/serial_protocol/serial_protocol.ino
@@ -5,64 +5,33 @@ const int pin_left    = 5;
 const int pin_right   = 6;
 
 // Initializing bit masks
-const int forward_mask = 8; // 1000
-const int reverse_mask = 4; // 0100
-const int left_mask    = 2; // 0010
-const int right_mask   = 1; // 0001
+const int forward_mask = 1 << 3; // 1000
+const int reverse_mask = 1 << 2; // 0100
+const int left_mask    = 1 << 1; // 0010
+const int right_mask   = 1 << 0; // 0001
 
-int protocol_num; // number of current state
-int forward_value = 0;
-int reverse_value = 0;
-int left_value = 0;
-int right_value = 0; // values that will be read from Serial after applying mask
-boolean commandComplete = false;
+int protocol_num = -1;
 
 void setup() {
-  pinMode(pin_forward, OUTPUT);
-  pinMode(pin_reverse, OUTPUT);
-  pinMode(pin_left,    OUTPUT);
-  pinMode(pin_right,   OUTPUT);
-
-  digitalWrite(pin_forward, HIGH);
-  digitalWrite(pin_reverse, HIGH);
-  digitalWrite(pin_left,    HIGH);
-  digitalWrite(pin_right,   HIGH);
-
-  Serial.begin(9600);
+    pinMode(pin_forward, OUTPUT);
+    pinMode(pin_reverse, OUTPUT);
+    pinMode(pin_left,    OUTPUT);
+    pinMode(pin_right,   OUTPUT);
+  
+    digitalWrite(pin_forward, HIGH);
+    digitalWrite(pin_reverse, HIGH);
+    digitalWrite(pin_left,    HIGH);
+    digitalWrite(pin_right,   HIGH);
+  
+    Serial.begin(9600);
 }
 
 void loop(){
-  if(commandComplete){
-    Serial.println();
-    Serial.println("Current move:");
-    Serial.println(protocol_num);
-    
-    forward_value = protocol_num & forward_mask;
-    reverse_value = protocol_num & reverse_mask;
-    left_value    = protocol_num & left_mask;
-    right_value   = protocol_num & right_mask;
-    
-    Serial.print(forward_value);
-    Serial.print(reverse_value);
-    Serial.print(left_value);
-    Serial.print(right_value);
-
-    digitalWrite(pin_forward, forward_value?LOW:HIGH);
-    digitalWrite(pin_reverse, reverse_value?LOW:HIGH);
-    digitalWrite(pin_left,    left_value?LOW:HIGH);
-    digitalWrite(pin_right,   right_value?LOW:HIGH);
-
-    commandComplete = false;
-  }
-}
-
-void serialEvent() {
-  if(Serial.available() > 0){
-    protocol_num = Serial.parseInt();
-  }
-  while(Serial.available() > 0){
-    Serial.read(); // clean up whatever left in Serial
-  }
-  commandComplete = true;
+    if((protocol_num = Serial.read()) == -1) return;
+  
+    digitalWrite(pin_forward, (protocol_num & forward_mask) ? LOW : HIGH);
+    digitalWrite(pin_reverse, (protocol_num & reverse_mask) ? LOW : HIGH);
+    digitalWrite(pin_left,    (protocol_num & left_mask) ? LOW : HIGH);
+    digitalWrite(pin_right,   (protocol_num & right_mask) ? LOW : HIGH);
 }
 
