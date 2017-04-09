a7d833fb8f08486605ededbd750c85acd7c5ed5b
Alena Lifar <Lifar_tut_net@mail.ru>
2017-04-09 18:00:00 +0300

    Revert "Fixes delay of serial input when using parseInt (#7)"
    This reverts commit 440675814c7acddded0066cdc88f473d5ad12e3f.

--- arduino/serial_protocol/serial_protocol.ino
+++ arduino/serial_protocol/serial_protocol.ino
@@ -5,34 +5,64 @@ const int pin_left    = 5;
 const int pin_right   = 6;
 
 // Initializing bit masks
-const int forward_mask = 1 << 3; // 1000
-const int reverse_mask = 1 << 2; // 0100
-const int left_mask    = 1 << 1; // 0010
-const int right_mask   = 1 << 0; // 0001
+const int forward_mask = 8; // 1000
+const int reverse_mask = 4; // 0100
+const int left_mask    = 2; // 0010
+const int right_mask   = 1; // 0001
 
-int protocol_num = -1;
+int protocol_num; // number of current state
+int forward_value = 0;
+int reverse_value = 0;
+int left_value = 0;
+int right_value = 0; // values that will be read from Serial after applying mask
+boolean commandComplete = false;
 
 void setup() {
-    pinMode(pin_forward, OUTPUT);
-    pinMode(pin_reverse, OUTPUT);
-    pinMode(pin_left,    OUTPUT);
-    pinMode(pin_right,   OUTPUT);
-  
-    digitalWrite(pin_forward, HIGH);
-    digitalWrite(pin_reverse, HIGH);
-    digitalWrite(pin_left,    HIGH);
-    digitalWrite(pin_right,   HIGH);
-  
-    Serial.begin(9600);
+  pinMode(pin_forward, OUTPUT);
+  pinMode(pin_reverse, OUTPUT);
+  pinMode(pin_left,    OUTPUT);
+  pinMode(pin_right,   OUTPUT);
+
+  digitalWrite(pin_forward, HIGH);
+  digitalWrite(pin_reverse, HIGH);
+  digitalWrite(pin_left,    HIGH);
+  digitalWrite(pin_right,   HIGH);
+
+  Serial.begin(9600);
 }
 
 void loop(){
-    if((protocol_num = Serial.read()) == -1) return;
+  if(commandComplete){
+    Serial.println();
+    Serial.println("Current move:");
+    Serial.println(protocol_num);
+    
+    forward_value = protocol_num & forward_mask;
+    reverse_value = protocol_num & reverse_mask;
+    left_value    = protocol_num & left_mask;
+    right_value   = protocol_num & right_mask;
+    
+    Serial.print(forward_value);
+    Serial.print(reverse_value);
+    Serial.print(left_value);
+    Serial.print(right_value);
 
-    // sets HIGH for no motion, and LOW to activate motion as we use ground pins to shorten the circuit
-    digitalWrite(pin_forward, (protocol_num & forward_mask) ? LOW : HIGH);
-    digitalWrite(pin_reverse, (protocol_num & reverse_mask) ? LOW : HIGH);
-    digitalWrite(pin_left,    (protocol_num & left_mask) ? LOW : HIGH);
-    digitalWrite(pin_right,   (protocol_num & right_mask) ? LOW : HIGH);
+    digitalWrite(pin_forward, forward_value?LOW:HIGH);
+    digitalWrite(pin_reverse, reverse_value?LOW:HIGH);
+    digitalWrite(pin_left,    left_value?LOW:HIGH);
+    digitalWrite(pin_right,   right_value?LOW:HIGH);
+
+    commandComplete = false;
+  }
+}
+
+void serialEvent() {
+  if(Serial.available() > 0){
+    protocol_num = Serial.parseInt();
+  }
+  while(Serial.available() > 0){
+    Serial.read(); // clean up whatever left in Serial
+  }
+  commandComplete = true;
 }
 
